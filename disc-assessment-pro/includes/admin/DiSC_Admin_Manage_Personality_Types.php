6b22e9c0e3a054f49d24950c6b3fc2a6dcc4a92d
serioha <63581810+serioha@users.noreply.github.com>
2024-07-30 23:53:10 +0300

    Update DiSC_Admin_Manage_Personality_Types to render personality types manager.

--- disc-assessment-pro/includes/admin/DiSC_Admin_Manage_Personality_Types.php
+++ disc-assessment-pro/includes/admin/DiSC_Admin_Manage_Personality_Types.php
@@ -5,10 +5,10 @@ class DiSC_Admin_Manage_Personality_Types extends DiSC_Admin_Base {
     }
 
     public function add_menu_item() {
-        add_submenu_page('disc-plugin', 'Manage Personality Types', 'Manage Personality Types', 'manage_options', 'disc-manage-personality-types', array($this, 'render_type_manager'));
+        add_submenu_page('disc-plugin', 'Manage Personality Types', 'Manage Personality Types', 'manage_options', 'disc-manage-personality-types', array($this, 'render_personality_types_manager'));
     }
 
-    public function render_type_manager() {
+    public function render_personality_types_manager() {
         // Render the personality type manager interface
     }
 }
