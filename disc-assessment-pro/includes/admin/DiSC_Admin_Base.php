964fc7db332f5f46442f991a10069d3bab5f26c0
serioha <63581810+serioha@users.noreply.github.com>
2024-07-31 14:54:53 +0300

    Add menu item functionality with submenu pages.

--- disc-assessment-pro/includes/admin/DiSC_Admin_Base.php
+++ disc-assessment-pro/includes/admin/DiSC_Admin_Base.php
@@ -7,12 +7,15 @@ abstract class DiSC_Admin_Base {
     }
 
     // Base class for admin functionality
-    public function add_menu_item() {
-        add_action('admin_menu', array($this, 'render_menu'));
+    public function add_menu_item($menu_title, $menu_slug, $capability, $menu_page, $callback) {
+        add_menu_page($menu_title, $menu_title, $capability, $menu_slug, $callback);
     }
 
     public function render_menu() {
-        // Add a menu item for the plugin
+        // Add submenu items
+        add_submenu_page('disc-assessment', 'Manage Tests', 'Manage Tests', 'manage_options', 'disc-manage-tests', array($this, 'render_test_manager'));
+        add_submenu_page('disc-assessment', 'Manage Personalities', 'Manage Personalities', 'manage_options', 'disc-manage-personality-types', array($this, 'render_personality_types_manager'));
     }
+
 }
 ?>
