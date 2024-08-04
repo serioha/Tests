0a738f17058a286b2865e1f97da7bbdee3f1b8d9
serioha <63581810+serioha@users.noreply.github.com>
2024-08-04 14:02:40 +0300

    feat: update question display and add create_question method in Create_Question class

--- disc-assessment-pro/includes/admin/create-question.php
+++ disc-assessment-pro/includes/admin/create-question.php
@@ -9,7 +9,18 @@ class Create_Question extends DiSC_Admin_Base {
     public function display_result() {
         // Display the Question with the answers
         echo '<h1>Question</h1>';
-        echo '<p>This is a test.</p>';
+        echo '<p>This is a question. The call from the `display_result` method.</p>';
+    }
+
+    public function create_question(){
+        // Create Question
+        echo '<p>These are the answers. The call from the `create_question` method.</p>';
+    }
+
+    public function render() {
+
+        echo '<h1>The rendered Question</h1>';
+        echo '<p>The question. This is the call from `render` method</p>';
     }
 }
 ?>
